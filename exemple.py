c3896fc0699032ad6b647b31b926cd791954de6f
titouan Jeannot <155440919+leskimou@users.noreply.github.com>
2025-04-18 14:20:32 +0900

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,13 +1,21 @@
-from preprocessing import Space_data
+#from preprocessing import Space_data
 from spaceeval import Space_Model
+import pandas as pd
 
-#download the data
+#---- space data -----
+
 # indicate the folder with the sportVU data (Rikako Kono shape)
 data_path = "data_VUNBA"
 
-Space_data(data_provider="SportVU_NBA", data_path= data_path).download_data()
+##for download the data
+#Space_data(data_provider="SportVU_NBA", data_path= data_path).download_data()
 
-basket_df = Space_data(data_provider="SportVU_NBA", data_path= data_path).preprocessing(nb_process_game = 4)
+##for reshape the data
+#basket_df = Space_data(data_provider="SportVU_NBA", data_path= data_path).preprocessing(nb_process_game = 4)
+
+
+#--------- space_eval -----------
+basket_df = pd.read_csv("data_VUNBA/basket_df.csv")
 
 # Initialize the sport and the model of space evaluation you want
 space_model = Space_Model('BIMOS')
@@ -16,7 +24,13 @@ space_model = Space_Model('BIMOS')
 data_frame = basket_df.iloc[[100]].copy()
 
 #chose a save path folder
-save_path_folder = "C:/Users/titou/Downloads"
+save_path_folder = data_path
 
-# Plot the heat map 
+# Plot the heat map for frame
 space_model.plot_heat_map_frame(save_path_folder, data_frame)
+
+# # Plot the heat map for sequence (very very long)
+# # filter only the row for the event we want to visualize
+# data_frame = basket_df[(basket_df['game_id'] == 1) & (basket_df['eventid']==6)].copy()
+
+# space_model.plot_heat_map_sequence(data_frame)
