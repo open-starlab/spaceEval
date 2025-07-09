183c8321ea59333cd45dc9ebc254dde37ea73e5b
leskimou <titouan.jeannot2@gmail.com>
2025-07-09 11:14:00 +0900

    new method get_values

--- spaceeval/sports/basketball/main_class_basketball/main.py
+++ spaceeval/sports/basketball/main_class_basketball/main.py
@@ -1,10 +1,10 @@
 from ..models.BIMOS import BIMOS
 from ..models.BMOS import BMOS
 import os
-import gdown
-import zipfile
-
+import tqdm
+import json
 from ..application.heatmap import plot_heat_map_frame, plot_heat_map_sequence
+
 
 class space_model_basketball:
     def __init__(self, model_name):
@@ -12,7 +12,6 @@ class space_model_basketball:
         Initializes the class with model name and ensures the required file is downloaded.
 
         :param model_name: str, the name of the model (e.g., "BIMOS", "BMOS")
-        :param file_id: str, the Google Drive file ID
         :param dest_path: str, the local path where the file will be saved
         """
         self.model_name = model_name
@@ -37,4 +36,47 @@ class space_model_basketball:
             model = "BMOS"
 
         plot_heat_map_sequence(model, data, save_path_folder,*args, **kwargs)
+    
+
+    def get_values(self, data, json_path=None):
+
+        if self.model_name == "BIMOS":
+            df_len = len(data)
+            print(f"Number of rows in the DataFrame: {df_len}")
+            result_dict = {}
+            for i in tqdm.tqdm(range(df_len)):
+                row = data.iloc[i]
+                key = f"{row['game']}_{row['attackid']}_{row['f_id']}"
+                value = BIMOS(data.iloc[[i]]).values.tolist()
+                result_dict[key] = value
+            
+            if json_path:
+                filename = "BIMOS_values.json"
+                full_path = os.path.join(json_path, filename)
+                
+                os.makedirs(json_path, exist_ok=True)
+                
+                with open(full_path, 'w') as f:
+                    json.dump(result_dict, f)
+            return result_dict
+        
+        if self.model_name == "BMOS":
+            df_len = len(data)
+            print(f"Number of rows in the DataFrame: {df_len}")
+            result_dict = {}
+            for i in tqdm.tqdm(range(df_len)):
+                row = data.iloc[i]
+                key = f"{row['game']}_{row['attackid']}_{row['f_id']}"
+                value = BMOS(data.iloc[[i]]).values.tolist()
+                result_dict[key] = value
+            
+            if json_path:
+                filename = "BMOS_values.json"
+                full_path = os.path.join(json_path, filename)
+                
+                os.makedirs(json_path, exist_ok=True)
+                
+                with open(full_path, 'w') as f:
+                    json.dump(result_dict, f)
+            return result_dict
 
