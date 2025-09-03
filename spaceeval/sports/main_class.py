f6a113377bce5f98c2888b8818aed1e5f68d6ef2
calvinyeungck <calvinyeungck@gmail.com>
2025-09-03 23:39:24 +0900

    update

--- spaceeval/sports/main_class.py
+++ spaceeval/sports/main_class.py
@@ -1,11 +1,15 @@
 class Space_Model:
     basketball_space_model = ['BIMOS','BMOS']
+    soccer_space_model = ['soccer_OBSO']
     other_model = []
 
     def __new__(cls, space_model, *args, **kwargs):
         if space_model in cls.basketball_space_model:
             from .basketball.main_class_basketball.main import space_model_basketball
             return space_model_basketball(space_model, *args, **kwargs)
+        elif space_model in cls.soccer_space_model:
+            from .soccer.soccer_main_func import space_model_soccer
+            return space_model_soccer(space_model, *args, **kwargs)
         elif space_model in cls.other_model:
             raise NotImplementedError('other model not implemented yet')
         else:
@@ -14,7 +18,7 @@ class Space_Model:
 if __name__ == '__main__':
     # import os
     # import pandas as pd
-    # data = pd.read_csv('C:/Users/titou/Desktop/starlab/all_data_reduce.csv')
+    # data = pd.read_csv('./starlab/all_data_reduce.csv')
     # # Filter data to keep only rows where game=1 and event_id=6
     # data_frame = data[(data['game'] == 1) & (data['eventid'] == 6)].copy()
     # # Reduce data_frame to only the first row
@@ -25,7 +29,7 @@ if __name__ == '__main__':
     # space_model = Space_Model('BIMOS')
 
     # #chose a save path folder
-    # save_path_folder = "C:/Users/titou/Downloads"
+    # save_path_folder = "./Downloads"
 
     # #Ensure the save pat directory exists
     # os.makedirs(save_path_folder, exist_ok=True)
